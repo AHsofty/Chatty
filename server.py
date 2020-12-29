0604ba6a716ea42e28bd5edc72c0d9809b5f53d4
Fruitloop <49094142+AHsofty@users.noreply.github.com>
2020-12-30 00:44:58 +0100

    Add files via upload
    Heyy

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
+"""
+So first off, I want to say that I'm not very familiar with socket programming.
+This is my first actual project I've worked on using sockets
+"""
+
+
 import socket
 import threading as t
-import time
 import pickle
-import time
 from Crypto.Cipher import AES
 
 """
 Do I regret not making a class? YES
-
 I kept adding stuff and things kept adding up, and now I have this :(
-
 """
 
-#IP = '85.214.165.121'
 IP = "localhost"
 PORT = 6969
 MaxAllowedInSingleRoom = 2
@@ -27,13 +28,13 @@ s.listen(5)
 """
 When encrypting, we want it to automatically change the encoding too
 So bytes <--> string
-
 We're using AES encryption
-
-NOTE: Currently the connection isn't end-to-end. Fuck
+NOTE: Currently the connection isn't end-to-end.
+I really should change this, but I cba.
 """
+
 def encrypt(txt):
-	obj = AES.new('You are a tosser'.encode("utf8"), AES.MODE_CFB, 'Aipom piplup 420'.encode("utf8"))
+	obj = AES.new('You are a tosser'.encode("utf8"), AES.MODE_CFB, 'Aipom piplup 420'.encode("utf8")) # Don't even question this.
 	ciphertext = obj.encrypt(pickle.dumps(txt))    
 	return ciphertext
 
@@ -60,7 +61,7 @@ def listen(conn, CurrentRoom):
 			I could've done this more efficiently by creating another function that does stuff
 			But I didn't because I'm lazy, please don't kill me
 
-			This bit needs optimization, but I CBA
+			This bit needs optimization
 			"""
 
 			# First we need to figure out who the inactive user is
@@ -77,8 +78,6 @@ def listen(conn, CurrentRoom):
 			# Now that we have got the active user, let's remove him from the room
 			del rooms[conn]
 
-			# Now that we have kicked the inactive user, let's notify all of the active users that somebody disconnected
-			# We now need to notify our current users' new name
 
 			# Ok now that there are only active users left, we need to notify them that somebody has disconnected
 			# We also need to notify them their new username
@@ -96,21 +95,22 @@ def listen(conn, CurrentRoom):
 				x.sendall(encrypt([f"Your updated name is: {idx+1}", None, 0]))
 			return
 
-		# ------------------------------------------------------------------------
-		
-		# If there is no error, we can continue
-		data = decrypt(data) # Current this is not end-to-end. Make it so the messages are in the form of a list. [0] = message; [1] = author
+
+		# ------------------------------------------------------------------------		
+		# If there is no error (so somebody didn't disconnect), we can continue
+		data = decrypt(data) # Current this is not end-to-end.
 
 		# Ok we've got the data, cool, but now we need to find which room is has to go to
 		# We can do this by checking which room belongs to the client, and sending the message 
 		# to all the clients in the same room
 
 		# Q: Wouldn't it be better if we made a server class? A: Yes
-		# Q: Then why aren't you doing that? A: LOL
+		# Q: Then why aren't you doing that? A: Please stop hurting my feelings
 
 		# We'll first get the room that matches the client that sent the message
-		for i in rooms.items():
+		for idx, i in enumerate(rooms.items()):
 			if i[0] == conn:
+				ussr = idx+1 # This is the user our message originates from
 				TargetRoom = i[1]
 				break
 
@@ -123,8 +123,9 @@ def listen(conn, CurrentRoom):
 				clients_in_room.append(i[0])		
 						
 
+		# Here we send the actual messages
 		for idx, x in enumerate(clients_in_room):
-			x.sendall(encrypt((data[0], idx+1, data[1]))) 
+			x.sendall(encrypt((data[0], ussr, data[1]))) 
 
 
 def assign_room(conn, RoomNumber):
@@ -136,7 +137,7 @@ def assign_room(conn, RoomNumber):
 		if i[1] == int(RoomNumber[0]):
 			num += 1
 			if num == MaxAllowedInSingleRoom:
-				# So if the max amount of people in a single room has been reached, we want to send an error back
+				# So if the max amount of people in a single room has been reached, we want to send an "error" back
 				# And disconnect the client from the server
 				print("max amount in a room has been reached")
 				conn.sendall(encrypt(["max_room", -1, 1])) # For some reason pickle decides to kill itself when I try to send back a tuple, not sure why
